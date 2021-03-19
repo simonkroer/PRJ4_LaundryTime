8911a2d9265e376860f1141ed1e9e624a77d8f0d
simonkroer <simonkroer@gmail.com>
2021-03-19 12:25:02 +0100

    UpdatedDb

--- LaundryTimeWebAppWithIdentity/Data/ApplicationDbContext.cs
+++ LaundryTimeWebAppWithIdentity/Data/ApplicationDbContext.cs
@@ -16,5 +16,7 @@ namespace LaundryTimeWebAppWithIdentity.Data
         {
 
         }
+
+
     }
 }
---
+++ LaundryTimeWebAppWithIdentity/Models/SystemAdmin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaundryTimeWebAppWithIdentity.Models
+{
+    public class SystemAdmin
+    {
+        public string Email { get; set; }
+    }
+}
