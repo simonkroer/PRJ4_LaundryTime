cf4e2155b59c7419df7e76295e359f1865d39d99
Egarder <61559853+Egarder@users.noreply.github.com>
2021-03-26 12:29:04 +0100

    added useradmin controller

--- LaundryTimeWebAppWithIdentity/Controllers/UserAdminController.cs
+++ LaundryTimeWebAppWithIdentity/Controllers/UserAdminController.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LaundryTimeWebAppWithIdentity.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LaundryTimeWebAppWithIdentity.Controllers
-{
-    public class UserAdminController
+{ public class UserAdminController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public UserAdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> MyUsers()
+        {
+            return View(await _context._UserAdmins.ToListAsync());
+        }
+
     }
 }
