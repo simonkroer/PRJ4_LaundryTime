e9a38e0e4c22f3d685a06bbdfe2950bee21f71af
Egarder <61559853+Egarder@users.noreply.github.com>
2021-03-26 12:23:11 +0100

    added

---
+++ LaundryTimeWebAppWithIdentity/Models/UserAdmin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace LaundryTimeWebAppWithIdentity.Models
+{
+    public class UserAdmin : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string PaymentMethod { get; set; }
+    }
+}
