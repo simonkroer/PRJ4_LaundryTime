a1e89cbc3e939528dd4bf29e68a7b31bfc7b6fe7
AlexanderWodstrup <Alexander.f.Wodstrup@gmail.com>
2021-04-07 14:59:42 +0200

    Fjernede kalender specifikke scripts load fra _layout

--- LaundryTimeWebAppWithIdentity/Controllers/CalenderController.cs
+++ LaundryTimeWebAppWithIdentity/Controllers/CalenderController.cs
@@ -3,6 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LaundryTimeWebAppWithIdentity.Models;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace LaundryTimeWebAppWithIdentity.Controllers
 {
@@ -12,5 +15,6 @@ namespace LaundryTimeWebAppWithIdentity.Controllers
         {
             return View();
         }
+
     }
 }
