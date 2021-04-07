f698def505c99b2df8b6a6a5664343c319050f01
AlexanderWodstrup <Alexander.f.Wodstrup@gmail.com>
2021-04-07 14:59:42 +0200

    Fjernede kalender specifikke scripts load fra _layout

---
+++ LaundryTimeWebAppWithIdentity/Models/BookingListModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LaundryTimeWebAppWithIdentity.Models
+{
+    public class BookingListModel
+    {
+        public IEnumerable<string> SelectedTimes { get; set; }
+        public IEnumerable<SelectListItem> Times { get; set; }  
+    }
+}
