b88f6ac3c32e9d4b58c9fe5dc85a4b4fa26e4ee5
AlexanderWodstrup <Alexander.f.Wodstrup@gmail.com>
2021-04-09 12:07:30 +0200

    DB chanbes

--- LaundryTimeWebAppWithIdentity/Models/User.cs
+++ LaundryTimeWebAppWithIdentity/Models/User.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace LaundryTimeWebAppWithIdentity.Models
 {
-    public class User : IdentityDbContext
+    public class User : IdentityUser
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
