972dab11fe5d8ad89a969e538ab6260892a9f879
AlexanderWodstrup <Alexander.f.Wodstrup@gmail.com>
2021-04-09 13:13:41 +0200

    Repositorys

---
+++ LaundryTimeWebAppWithIdentity/Repositorys/Repository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using LaundryTimeWebAppWithIdentity.Data;
+
+namespace LaundryTimeWebAppWithIdentity.Repositorys
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly ApplicationDbContext _context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+
+        public TEntity Get(int id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _context.Set<TEntity>().Where(predicate);
+        }
+
+        public TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _context.Set<TEntity>().SingleOrDefault(predicate);
+        }
+
+
+    }
+}
---
+++ LaundryTimeWebAppWithIdentity/Repositorys/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LaundryTimeWebAppWithIdentity.Data;
+using LaundryTimeWebAppWithIdentity.Models.Calender;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaundryTimeWebAppWithIdentity.Repositorys
+{
+    public class UserRepository : Repository<UserModel>, IUserRepository
+    {
+        public UserRepository(ApplicationDbContext context) : base(context) { }
+
+        public IEnumerable<UserModel> GetUsersReservedBookings()
+        {
+            return _context.Users.Include(u=>u.reservedBooking).ToList();
+        }
+    }
+}
