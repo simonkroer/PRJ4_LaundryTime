b9c2306f092c456adc153b806a273ff3024f5ac2
pallemanden <58727279+pallemanden@users.noreply.github.com>
2021-04-09 13:13:19 +0200

    IUnitOfWork

---
+++ LaundryTimeWebAppWithIdentity/Unit of Work/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaundryTimeWebAppWithIdentity.Unit_of_Work
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        int Complete();
+
+    }
+}
