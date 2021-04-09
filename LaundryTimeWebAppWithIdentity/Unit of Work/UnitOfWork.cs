8aa342a943042c4e3ed2400ebf4625730e558026
AlexanderWodstrup <Alexander.f.Wodstrup@gmail.com>
2021-04-09 13:33:30 +0200

    It worked i guess

---
+++ LaundryTimeWebAppWithIdentity/Unit of Work/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LaundryTimeWebAppWithIdentity.Data;
+using LaundryTimeWebAppWithIdentity.Models;
+using Microsoft.Extensions.Hosting.Internal;
+
+namespace LaundryTimeWebAppWithIdentity.Unit_of_Work
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
