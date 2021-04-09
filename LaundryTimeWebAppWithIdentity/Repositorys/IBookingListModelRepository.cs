5d874e9af33e1f169e85d2a07299b0fd4f278a0b
AlexanderWodstrup <Alexander.f.Wodstrup@gmail.com>
2021-04-09 15:20:41 +0200

    Datebase API

---
+++ LaundryTimeWebAppWithIdentity/Repositorys/IBookingListModelRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LaundryTimeWebAppWithIdentity.Models.Calender;
+
+namespace LaundryTimeWebAppWithIdentity.Repositorys
+{
+    public interface IBookingListModelRepository : IRepository<BookingListModel>
+    {
+        public IEnumerable<BookingListModel> GetBookingListWithDate();
+    }
+}
--- LaundryTimeWebAppWithIdentity/Unit of Work/UnitOfWork.cs
+++ LaundryTimeWebAppWithIdentity/Unit of Work/UnitOfWork.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using LaundryTimeWebAppWithIdentity.Data;
 using LaundryTimeWebAppWithIdentity.Models;
+using LaundryTimeWebAppWithIdentity.Repositorys;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting.Internal;
 
 namespace LaundryTimeWebAppWithIdentity.Unit_of_Work
@@ -12,9 +14,16 @@ namespace LaundryTimeWebAppWithIdentity.Unit_of_Work
     {
         private readonly ApplicationDbContext _context;
 
+        public IUserRepository Users { get; private set; }
+        public IBookingListModelRepository BookingLists { get; private set; }
+        public IDateModelRepository DateModelLists { get; private set; }
+
         public UnitOfWork(ApplicationDbContext context)
         {
             _context = context;
+            Users = new UserRepository(_context);
+            BookingLists = new BookingListModelRepository(_context);
+            DateModelLists = new DateModelRepository(_context);
         }
         public int Complete()
         {
