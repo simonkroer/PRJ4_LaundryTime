d7719dd450a8c2be1b105e4ce523a9ffc0282f08
Egarder <61559853+Egarder@users.noreply.github.com>
2021-04-10 21:23:29 +0200

    aa

--- LaundryTimeWebAppWithIdentity/Models/Machine.cs
+++ LaundryTimeWebAppWithIdentity/Models/Machine.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,6 +16,7 @@ namespace LaundryTimeWebAppWithIdentity.Models
 
         public DateTime InstallationDateTime { get; set; }
 
+        [ForeignKey("UserAdminId")]
         public UserAdmin owner { get; set; }
     }
 }
--- LaundryTimeWebAppWithIdentity/Models/UserAdmin.cs
+++ LaundryTimeWebAppWithIdentity/Models/UserAdmin.cs
@@ -10,6 +10,8 @@ namespace LaundryTimeWebAppWithIdentity.Models
 {
     public class UserAdmin : IdentityUser
     {
+        [Key]
+        public int UserAdminId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
