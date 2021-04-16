3db24a5c41594571fe165421588123438b888f5d
Egarder <61559853+Egarder@users.noreply.github.com>
2021-04-16 12:28:04 +0200

    dd

--- LaundryTime/Data/IDataAccessAction.cs
+++ LaundryTime/Data/IDataAccessAction.cs
@@ -8,14 +8,13 @@ namespace LaundryTime.Data
 {
     public interface IDataAccessAction : IDisposable
     {
-        IMachineRepository Machines { get; }
-        IAddressRepository Address { get; }
+        public IMachineRepository Machines { get; }
         public ILaundryUserRepository LaundryUsers { get; }
 
         public IUserAdminRespository UserAdmins { get; }
 
         public ISystemAdminRepository SystemAdmin { get; }
-        IAddressRepository Addresses { get; }
+        public IAddressRepository Addresses { get; }
 
         int Complete();
     }
