8321e2ec6904fe40fd431f8f491ae126a7bdef40
Egarder <61559853+Egarder@users.noreply.github.com>
2021-04-16 10:54:19 +0200

    added new final project for user setup bottom up

---
+++ LaundryTime/Data/Models/UserAdmin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using LaundryTime.Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace LaundryTime.Data.Models
+{
+    public class UserAdmin: IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string PaymentMethod { get; set; }
+
+        [ForeignKey("AddressId")]
+        public Address Address { get; set; }
+
+        private List<IdentityUser> Users { get; set; }
+
+        
+        private List<Machine> Machines { get; set; }
+
+
+
+    }
+}
