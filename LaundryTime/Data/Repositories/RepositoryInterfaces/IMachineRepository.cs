c90867e730911afcfd5b279cef3fa74e88edf337
AlexanderWodstrup <Alexander.f.Wodstrup@gmail.com>
2021-04-23 14:16:05 +0200

    Migrations and Machine seeder

--- LaundryTime/Data/Repositories/RepositoryInterfaces/IMachineRepository.cs
+++ LaundryTime/Data/Repositories/RepositoryInterfaces/IMachineRepository.cs
@@ -9,9 +9,9 @@ namespace LaundryTime.Data.Repositories.RepositoryInterfaces
     public interface IMachineRepository : IRepository<Machine>
     {
         List<Machine> GetAllMachines();
-        Machine GetSingleMachine(string id);
+        Machine GetSingleMachine(int id);
         void AddMachine(Machine machine);
         int GetNumberOfMachines();
-
+        public bool MachineExist(string number);
     }
 }
