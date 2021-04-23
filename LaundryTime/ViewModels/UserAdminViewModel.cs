de76f3060d4fe9ea99b3e3e2071e7137cbc20aba
Egarder <61559853+Egarder@users.noreply.github.com>
2021-04-23 11:57:43 +0200

    added

--- LaundryTime/ViewModels/UserAdminViewModel.cs
+++ LaundryTime/ViewModels/UserAdminViewModel.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LaundryTime.Data.Models;
 
 namespace LaundryTime.ViewModels
 {
     public class UserAdminViewModel
     {
+        public List<LaundryUser> MyUsers { get; set; }
+
+
     }
 }
