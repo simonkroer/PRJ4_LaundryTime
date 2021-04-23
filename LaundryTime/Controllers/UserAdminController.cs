3119a1bfb4a1121411591051e7a08ba5c3781207
Egarder <61559853+Egarder@users.noreply.github.com>
2021-04-23 11:57:43 +0200

    added

--- LaundryTime/Controllers/UserAdminController.cs
+++ LaundryTime/Controllers/UserAdminController.cs
@@ -3,11 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LaundryTime.Data;
+using LaundryTime.Data.Models;
 
 namespace LaundryTime.Controllers
 {
     public class UserAdminController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private IDataAccessAction _dataAccess;
+
+        UserAdminController(ApplicationDbContext context)
+        {
+            _context = context;
+            _dataAccess = new DataAccsessAction(context);
+        }
         public IActionResult Index()
         {
             return View();
