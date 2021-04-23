4ef02de0cdd5f7e9fc5410624c81218138c4e002
AlexanderWodstrup <Alexander.f.Wodstrup@gmail.com>
2021-04-23 12:43:01 +0200

    Stuff

---
+++ LaundryTime/Data/Models/Booking/ReservedListModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaundryTime.Data.Models.Booking
+{
+    public class ReservedListModel
+    {
+        public int Id { get; set; }
+        public string Time { get; set; }
+        public string Machine { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+    }
+}
