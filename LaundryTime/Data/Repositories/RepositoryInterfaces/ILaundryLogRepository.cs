dfaa17ffb99cdb3882dc0accce3bd18e2445ef2e
Camilla <camilla.holmstoel@hotmail.com>
2021-04-30 13:35:28 +0200

    LaundryLogs added

---
+++ LaundryTime/Data/Repositories/RepositoryInterfaces/ILaundryLogRepository.cs
@@ -0,0 +1,13 @@
+﻿using LaundryTime.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaundryTime.Data.Repositories.RepositoryInterfaces
+{
+    public interface ILaundryLogRepository: IRepository<LaundryLog>
+    {
+        bool LaundryLogExists(string id);
+    }
+}
