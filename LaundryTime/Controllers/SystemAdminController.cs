4973e3e614734e5fe77b0d394ed3845cb67a2542
Jim <partyblues@gmail.com>
2021-05-03 16:17:34 +0200

    Admin4

--- LaundryTime/Controllers/SystemAdminController.cs
+++ LaundryTime/Controllers/SystemAdminController.cs
@@ -3,13 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LaundryTime.Data;
+using LaundryTime.ViewModels;
 
 namespace LaundryTime.Controllers
 {
 
-
-    public class SystemAdminController : Controller
+	public class SystemAdminController : Controller
     {
+	    private readonly ApplicationDbContext _context;
+	    private IDataAccessAction _dataAccess;
+	    public SystemAdminViewModel _systemAdminViewModel;
+
+	    public SystemAdminController(ApplicationDbContext context)
+	    {
+		    _context = context;
+		    _dataAccess = new DataAccsessAction(context);
+		    _systemAdminViewModel = new SystemAdminViewModel();
+	    }
+
+
 
         public IActionResult Index()
         {
--- LaundryTime/ViewModels/SystemAdminViewModel.cs
+++ LaundryTime/ViewModels/SystemAdminViewModel.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LaundryTime.Data.Models;
 
 namespace LaundryTime.ViewModels
 {
     public class SystemAdminViewModel
     {
+        public SystemAdminViewModel() { }
+
+        public List<UserAdmin> MyUserAdmins { get; set; }
     }
 }
