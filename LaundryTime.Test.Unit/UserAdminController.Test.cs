9151ddbc3bfdc08f5a42204a82890b0a1e09a4fe
Egarder <61559853+Egarder@users.noreply.github.com>
2021-05-13 08:58:22 +0200

    dd

--- LaundryTime.Test.Unit/UserAdminController.Test.cs
+++ LaundryTime.Test.Unit/UserAdminController.Test.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LaundryTime.Data;
+using LaundryTime.ViewModels;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 
 namespace LaundryTime.Test.Unit
 {
     public class UserAdminController
     {
+        protected ApplicationDbContext _context { get; set; }
+        protected UserAdminController _userAdminController;
+        private IDataAccessAction _dataAccess;
+        public UserAdminViewModel _userAdminViewModel;
+
         [SetUp]
         public void Setup()
         {
+            _context = new ApplicationDbContext(
+                new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlite(CreateInMemoryDatabase()).Options);
+            _userAdminController = new UserAdminController();
         }
 
         [Test]
@@ -20,5 +33,12 @@ namespace LaundryTime.Test.Unit
             Assert.Pass();
         }
 
+
+        static DbConnection CreateInMemoryDatabase()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");//Fake db
+            connection.Open();
+            return connection;
+        }
     }
 }
