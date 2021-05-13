ac56f30a6b425ccf58cba6d749fd20b67ec2be22
Egarder <61559853+Egarder@users.noreply.github.com>
2021-05-13 08:51:47 +0200

    dd

---
+++ LaundryTime.Test.Unit/UserAdminController.Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LaundryTime.Test.Unit
+{
+    public class UserAdminController
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
