d610e8bd854143097fe33dbf0ac690aa8061bc8c
Jim <partyblues@gmail.com>
2021-05-21 15:55:09 +0200

    Jim

--- LaundryTime/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LaundryTime/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -127,8 +127,29 @@ namespace LaundryTime.Areas.Identity.Pages.Account
                             values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
                             protocol: Request.Scheme);
 
-                        await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                        MailMessage message = new MailMessage()
+                        {
+                            From = new MailAddress("laundrytime@outlook.dk"), // sender must be a full email address
+                            Subject = "Please confirm your e-mail",
+                            IsBodyHtml = true,
+                            Body = $"<h3>Hello {user.Name}</h3><p>Thank you for registering with Laundry Time!</p> " +
+                                   $"<p>Below you will find your user information:</p> <p>User name: {user.Email} </p> " +
+                                   $"<p>Password: {Input.Password} </p> " +
+                                   $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>. " +
+                                   $"<br/> <br/> <img width='100' src='https://t4.ftcdn.net/jpg/03/09/29/23/360_F_309292393_4G7XxgXz5ftKSuSStItdT2ZK1snVEH08.jpg'/> <p>Kind regards</p> <p>Laundry Time</p>",
+                            BodyEncoding = System.Text.Encoding.UTF8,
+                            SubjectEncoding = System.Text.Encoding.UTF8,
+                            To = { user.Email }
+                        };
+
+                        string smsMsg =
+                            $"Hi {user.Name}!\n\nThank you for registering with Laundry Time!\n" +
+                            $"Below you will find your user information:\nUser name: {user.Email}" +
+                            $"\nPassword: {Input.Password}\n\n" +
+                            $"Please remember to confirm your account by clicking the link in the mail sent to {user.Email}";
+
+                          SendMail(message);
+                          SendSMS(user.PhoneNumber, smsMsg);
 
                         if (User.Identity != null)
                         {
@@ -147,18 +168,56 @@ namespace LaundryTime.Areas.Identity.Pages.Account
                             return LocalRedirect(returnUrl);
                         }
                     }
-                    foreach (var error in result.Errors)
-                    {
-                        ModelState.AddModelError(string.Empty, error.Description);
-                    }
+
+	                    foreach (var error in result.Errors)
+	                    {
+	                        ModelState.AddModelError(string.Empty, error.Description);
+	                    }
                 }
 
                 //Til systemadmin til at oprette de forskellige typer brugere
                 if (User.HasClaim("SystemAdmin", "IsSystemAdmin"))
                 {
+	                var user = new UserAdmin() { UserName = Input.Email, Email = Input.Email, Name = Input.Name, 
+		                WorkAddress = new Address() { StreetAddress = Input.StreetAddress, Zipcode = Input.Zipcode },
+		                PhoneNumber = Input.Phonenumber,
+		                PaymentMethod = Input.PaymentMethod
+	                };
 
+	                var result = await _userManager.CreateAsync(user, Input.Password);
+
+	                if (result.Succeeded)
+	                {
+		                _logger.LogInformation("UserAdmin is created! ");
+	                }
+
+                    //=====================================================Hvad sker der her?==================================================================
+
+	                if (User.Identity != null)
+	                {
+		                var useradmin = _dataAccess.UserAdmins.GetSingleUserAdmin(User.Identity.Name);
+		                useradmin.Users.Add(user);
+		                _dataAccess.Complete();
+	                }
+
+	                if (_userManager.Options.SignIn.RequireConfirmedAccount)
+	                {
+		                return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+	                }
+	                else
+	                {
+		                await _signInManager.SignInAsync(user, isPersistent: false);
+		                return LocalRedirect(returnUrl);
+	                }
                 }
-                else
+                //foreach (var error in result.Errors)
+                //{
+                //	ModelState.AddModelError(string.Empty, error.Description);
+                //}
+                //============================================================Hvad sker der her?===================================================================
+
+            
+            else
                 {
                     var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
                     var result = await _userManager.CreateAsync(user, Input.Password);
--- LaundryTime/Controllers/SystemAdminController.cs
+++ LaundryTime/Controllers/SystemAdminController.cs
@@ -12,53 +12,50 @@ namespace LaundryTime.Controllers
 {
 
 	public class SystemAdminController : Controller
-    {
-	    private readonly ApplicationDbContext _context;
-	    private IDataAccessAction _dataAccess;
-	    public SystemAdminViewModel _systemAdminViewModel;
+	{
+		private readonly ApplicationDbContext _context;
+		private IDataAccessAction _dataAccess;
+		public SystemAdminViewModel _systemAdminViewModel;
 
-	    public SystemAdminController(ApplicationDbContext context)
-	    {
-		    _context = context;
-		    _dataAccess = new DataAccsessAction(context);
-		    _systemAdminViewModel = new SystemAdminViewModel();
-	    }
+		public SystemAdminController(ApplicationDbContext context)
+		{
+			_context = context;
+			_dataAccess = new DataAccsessAction(context);
+			_systemAdminViewModel = new SystemAdminViewModel();
+		}
 
 
 
-        public IActionResult Index()
-        {
-	        if (User.HasClaim("SystemAdmin", "IsSystemAdmin"))
-	        {
-
-		        
+		public IActionResult Index()
+		{
+			if (User.HasClaim("SystemAdmin", "IsSystemAdmin"))
+			{
 				_systemAdminViewModel.AllUserAdmins = _dataAccess.UserAdmins.GetAllUserAdmins();
-		        
-		        return View(_systemAdminViewModel);
-	        }
 
-	        return Unauthorized();
-        }
+				return View(_systemAdminViewModel);
+			}
+			return Unauthorized();
+		}
 
-        // Post /account/Register
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Register(UserAdmin userAdmin)
-        {
+		//// Post /account/Register
+		//[HttpPost]
+		//[ValidateAntiForgeryToken]
+		//public async Task<IActionResult> Register(UserAdmin userAdmin)
+		//{
 
-	        if (ModelState.IsValid)
-	        {
-		        var appUser = new UserAdmin()
-		        {
-			        Name = userAdmin.Name,
-					PaymentMethod = userAdmin.PaymentMethod,
-		        };
-		      
-		        _dataAccess.UserAdmins.AddUserAdmin(appUser);
-		        
-		       
-	        }
-	        return View(user);
-        }
+		//	if (ModelState.IsValid)
+		//	{
+		//		var appUser = new UserAdmin()
+		//		{
+		//			Name = userAdmin.Name,
+		//			PaymentMethod = userAdmin.PaymentMethod,
+		//		};
+
+		//		_dataAccess.UserAdmins.AddUserAdmin(appUser);
+
+
+		//	}
+		//	return View(user);
+		//}
 	}
 }
