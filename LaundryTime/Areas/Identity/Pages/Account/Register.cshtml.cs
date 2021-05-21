41a45414a10909423be3e72443a19517d0b1d081
Jim <partyblues@gmail.com>
2021-05-21 15:55:46 +0200

    Jimbo

--- LaundryTime/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ LaundryTime/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
